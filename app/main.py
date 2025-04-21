cfea012657cd5050c0eb999317abd36f5765a507
Matheus Gama <matt.backup821@gmail.com>
2025-04-21 12:12:47 +0100

    feat(Inclusao-de-uma-nova-rota-webhook): inclusao de uma nova rota webhook
    Rota foi adicionada apenas para receber payloads e sempre retornar um status positivo. Rota apenas para testes
    
    BREAKING CHANGE:

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from app.router import webook
 
 app = FastAPI()
 
@@ -8,3 +8,6 @@ app = FastAPI()
 @app.get('/')
 async def main():
     return {'message': 'Python is beautiful', 'status': 'OK'}
+
+
+app.include_router(webook.router, prefix='/api/v1')
